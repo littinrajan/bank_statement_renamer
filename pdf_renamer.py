44aee76acb98a5f2b3dc83db77ebfe34ce7a06c5
littinrajan <littinrajan@gmail.com>
2022-08-06 23:20:07 +0530

    added libraries

--- pdf_renamer.py
+++ pdf_renamer.py
@@ -2,3 +2,14 @@
 import re
 import os
 from datetime import datetime
+
+def read_pdf(pdf_path):
+    # reading pdffile
+    pdf_obj = open(pdf_path, 'rb')
+    # creating pdf reader  object
+    pdf_reader = PdfFileReader(pdf_obj)
+    # getting total no. of pages present
+    total_pages = pdf_reader.numPages
+    # extracting pdf data
+    pdf_data = "\n\n".join([pdf_reader.getPage(i).extractText() for i in range(total_pages)])
+    return pdf_data

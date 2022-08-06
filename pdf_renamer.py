cf6f233ec8650fa4b047e80ac781094e18abbf14
littinrajan <littinrajan@gmail.com>
2022-08-06 23:23:13 +0530

    added format function

--- pdf_renamer.py
+++ pdf_renamer.py
@@ -16,3 +16,42 @@ def read_pdf(pdf_path):
     # closing file object
     pdf_obj.close()
     return pdf_data
+    
+    
+def format_filename(pdf_path):
+    # checking file existence as well as extension
+    if not os.path.isfile(pdf_path):
+        print('File not existing')
+    else:
+        # checking file extension
+        if os.path.splitext(pdf_path.lower())[-1] != '.pdf':
+            print('File is not a PDF type')
+        else:
+            try:
+                # extracting pdf_data
+                pdf_data = read_pdf(pdf_path)
+
+                # setting bank name
+                bank_name = 'CBA-SA'
+                # getting account number
+                acc_num = re.findall("Account Number\n.*\d{8}", pdf_data)[0].split()[-1][-4:]
+                # getting name
+                name = re.findall("Name:\n.*", pdf_data)[0].split('\n')[-1]
+                name = re.sub('-', ' ', name).title()
+                # getting closing balance
+                bal = re.findall("Closing Balance\n.*", pdf_data)[0].split('\n')[-1][:-3]
+                # getting start and end period
+                period = re.findall("Period\n.*", pdf_data)[0].split('\n')[-1]
+                period_start, period_end = [date_time_formatter(date) for date in period.split(' - ')]
+
+                # recreating filename
+                filename_new = "-".join([bank_name, bal, acc_num, name, period_start, period_end]) + '.pdf'
+                path_actual = Path(pdf_path)
+                # renaming file
+                try:
+                    path_actual.rename(Path(path_actual.parent, f"{filename_new}"))
+                    print('> Successfully renamed')
+                except:
+                    print('> Error while renaming')
+            except:
+                print('> Error while renaming')
